567ed18093c59e75d89b61d67d94b0d4521b2b96
Filipe Almeida <filipe.lmd@gmail.com>
2017-08-23 00:18:08 +0100

    add json bindings

--- types/types.go
+++ types/types.go
@@ -17,31 +17,31 @@ type BaseEntity struct {
 // MonsterEntity describes the general stats of a Monster
 type MonsterEntity struct {
 	BaseEntity
-	Strength            StatPair
-	Dexterity           StatPair
-	Constitution        StatPair
-	Intelligence        StatPair
-	Wisdom              StatPair
-	Charisma            StatPair
-	Challenge           int
-	ArmorClass          int
-	HitPoints           int
-	Speed               int
-	DamageResistances   []resistance
-	DamageImmunities    []immunity
-	ConditionImmunities []immunity
-	Sense               []sense
-	Languages           []string
-	Habilities          []hability
-	Actions             []action
-	Description         string
-	Picture             string
+	Strength            StatPair     `json:"strength"`
+	Dexterity           StatPair     `json:"dexterity"`
+	Constitution        StatPair     `json:"constitution"`
+	Intelligence        StatPair     `json:"intelligence"`
+	Wisdom              StatPair     `json:"wisdom"`
+	Charisma            StatPair     `json:"charisma"`
+	Challenge           int          `json:"challenge"`
+	ArmorClass          int          `json:"armor_class"`
+	HitPoints           int          `json:"hit_points"`
+	Speed               int          `json:"speed"`
+	DamageResistances   []resistance `json:"damage_resistances"`
+	DamageImmunities    []immunity   `json:"damage_immunities"`
+	ConditionImmunities []immunity   `json:"condition_immunities"`
+	Sense               []sense      `json:"sense"`
+	Languages           []string     `json:"languages"`
+	Habilities          []hability   `json:"habilities"`
+	Actions             []action     `json:"actions"`
+	Description         string       `json:"description"`
+	Picture             string       `json:"picture"`
 }
 
 // StatPair describes a base attribute (such as strength) and its modifier
 type StatPair struct {
-	Attribute int
-	Modifier  int
+	Attribute int `json:"attribute"`
+	Modifier  int `json:"modifier"`
 }
 
 // TODO: further specify habilities, resistances, immunities and senses
@@ -52,8 +52,8 @@ type sense struct{}
 type hability struct{}
 type action struct{}
 
-// MonsterResult describes a an HTTP call response when fetching monsters
-type MonsterResult struct {
+// Result describes a an HTTP call response when fetching monsters
+type Result struct {
 	Items       []MonsterEntity
 	TotalCount  int64
 	CurrentPage int
